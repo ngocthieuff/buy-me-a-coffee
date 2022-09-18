afd2f5c30090ab8cad7db6e070131eef1aa3b13d
ngocthieuff <ptnngoc22@gmail.com>
2022-09-18 23:31:48 +0700

    display current acc

--- client/src/components/atoms/Welcome.tsx
+++ client/src/components/atoms/Welcome.tsx
@@ -63,8 +63,8 @@ const Welcome = () => {
                 </div>
 
 
-                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-                        <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+                <div className="flex flex-col flex-1 items-center justify-start w-full">
+                        <div className="p-3 justify-end items-start flex-col rounded-xl h-40 w-full my-5 eth-card white-glassmorphism" style={{width: '343px'}}>
                             <div className="flex justify-between flex-col w-full h-full">
                                 <div className="flex justify-between items-start">
                                     <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -73,10 +73,12 @@ const Welcome = () => {
                                     <BsInfoCircle fontSize={17} color="#fff" />
                                 </div>
                                 <div>
-                                    <p className="text-white font-light text-sm">
-                                    0xNgocccccccccc
+                                    <p className="text-white text-sm font-normal">
+                                    {
+                                        context?.currentAccount ? context?.currentAccount : '0xNgocccccccccc'
+                                    }
                                     </p>
-                                    <p className="text-white font-semibold text-lg mt-1">
+                                    <p className="text-white font-semibold text-md mt-1">
                                     Ethereum
                                     </p>
                                 </div>
