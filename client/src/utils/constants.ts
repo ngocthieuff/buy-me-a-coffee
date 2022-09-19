80ad7fe251adf78b2e65b9ca8a83b7687f0c504d
ngocthieuff <ptnngoc22@gmail.com>
2022-09-19 10:27:57 +0700

    use goerli testnet instead

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,9 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi;
-export const contractAddress = '0x81d1F7bEAA00B33d5850b4fa2B8337AF55B3bA59';
+
+// vng fresher chain
+// export const contractAddress = '0x81d1F7bEAA00B33d5850b4fa2B8337AF55B3bA59';
+
+// goerli test net
+export const contractAddress = '0x6787eBF195Cc39012562e4651d2E43281136387b';
